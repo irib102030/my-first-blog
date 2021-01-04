59c25c08a144098eb2437a47f4add1586d9a25a5
Ali.m <irib102030@gmail.com>
2021-01-04 23:10:30 +0330

    a field added and removed. not a big deal

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    editor = models.ForeignKet('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
